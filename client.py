95604f9f096f91c26dd87d86166d37098b331a98
DeAntoLei <37049845+DeAntoLei@users.noreply.github.com>
2021-10-17 21:23:17 +0300

    Initial commit
    Added server application file
    Added client application file

---
+++ client.py
@@ -0,0 +1,94 @@
+# client.py application
+# Sends messages with random message types to a server every 1 second until it receives end-of-messssge character
+# Simulates the functionality of an MQTT client that publishes and subscribes to a topic using TCP/IP protocol
+# The client terminates if it fails to establish a connection or if publishing or subscribing actions fail
+
+import sys, random, time, socket, pickle
+
+#
+TCP_IP = "127.0.0.1"
+TCP_PORT = 5010
+BUFFER_SIZE = 4096
+CLIENT_MESSAGE_TYPES = ["onBody", "offBody", "brfChange", "enuEvent"]
+
+class message:
+    def __init__(self, *args):
+        self.seqNum = args[0]
+        self.msgType = args[1]
+        self.timestamp = time.time()
+        if len(args) == 3:
+            self.deviceId = args[2]
+
+# Function that securely establishes a connection to a server
+def connect():
+    global sock
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print ("Connecting to %s:%d ..." % (TCP_IP, TCP_PORT))
+    try:
+        sock.connect((TCP_IP, TCP_PORT))
+    except:
+        raise Exception("Connection to %s:%d failed" % (TCP_IP, TCP_PORT))
+
+# Function that closes a connection to a server
+def closeConnection():
+    global isConnected
+    print ("Closing socket")
+    sock.close()
+    isConnected = False
+
+# Function that enables the client to send a message to a server 
+# It simulates the case when a client publishes to a topic by sending a message to a connected server
+def publishMessage(msg):
+    try:
+        sock.send(msg)
+    except:
+        raise Exception("Exception in sock.send()")
+
+# Function that securely receives data from an active server-client connection 
+# It simulates the reception of a message by the broker, on a topic the client is subscribed to 
+def getSubscriptionMessage():
+    try:
+        return sock.recv(BUFFER_SIZE)
+    except:
+        raise Exception("Exception from blocking sock.recv()")
+ 
+# Socket initialization
+sock = None
+isConnected = False
+
+try:
+    # Establishing server-client connection
+    connect()
+    
+    isConnected = True
+    print ("Connection established")
+    time.sleep(1)
+    
+    msgSeqNum = 0        
+    response = None
+    while isConnected and response != "/0":
+        msgSeqNum += 1
+        msg = message(msgSeqNum, CLIENT_MESSAGE_TYPES[random.randint(0,3)], random.randint(0, 1000))
+        print ("Sending message to server: ", msg.deviceId, msg.seqNum, msg.msgType, msg.timestamp)
+           
+        try:
+            # Sending a message to the server
+            publishMessage(pickle.dumps(msg))
+            
+            # Server response reception
+            response = pickle.loads(getSubscriptionMessage())
+            print ("Received message from server: ", response.seqNum, response.msgType, response.timestamp)
+        except Exception as e:
+            print (e)
+            closeConnection()
+            break
+            
+        time.sleep(1)
+        
+    time.sleep(1)
+except Exception as e:
+    print (e)
+
+# Client termination    
+print ("Client application will be terminated...")
+sys.exit()
---
+++ server.py
@@ -0,0 +1,92 @@
+# server.py application
+# Receives client messages and responds with random message types
+# Simulates the funtionality of an MQTT broker using TCP/IP protocol and sockets
+# The server can serve only 1 connection. Only terminates if socket binding fails
+# Waits infinitely for a client to connect to its socket
+
+import sys, random, time, socket, pickle
+
+# Definition of constants
+TCP_IP = "127.0.0.1"
+TCP_PORT = 5010
+BUFFER_SIZE = 4096
+SERVER_MESSAGE_TYPES = ["erc", "drc", "res"]
+
+class message:
+    def __init__(self, *args):
+        self.seqNum = args[0]
+        self.msgType = args[1]
+        self.timestamp = time.time()
+        if len(args) == 3:
+            self.deviceId = args[2]
+
+# Function that securely binds a socket to an address
+def bind():
+    global sock
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print ("Binding to %s:%d ..." % (TCP_IP, TCP_PORT))
+    try:
+        sock.bind((TCP_IP, TCP_PORT))
+    except socket.error as msg:
+        raise Exception("Bind to %s:%d failed" % (TCP_IP, TCP_PORT), msg[0], msg[1])
+    
+# Function that securely receives data from an active server-client connection 
+# It simulates the reception of a message, published by a client to a topic, on the broker side 
+def recvTopicInfo():
+    try:
+        return conn.recv(BUFFER_SIZE)
+    except:
+        raise Exception ("Exception from conn.recv()")
+
+# Function that securely sends data through an accepted connection
+# It simulates the state where the broker forwards a message on a topic to a subscribed client
+def serveTopicSubscription(msg):
+    try:
+        conn.send(msg)
+    except:
+        raise Exception ("Exception from conn.send()")
+
+# Socket initialization
+sock = None
+
+try:
+    # Socket binding
+    bind()
+    # Server is enabled to accept 1 connection
+    sock.listen()
+
+    while True:
+        print ("Waiting for a connection client...")
+        # The server accepts a connection
+        conn, addr = sock.accept()
+        print ("Connected to a client at ", addr[0])
+        
+        # Message exchange between server and client
+        msgSeqNum = 0
+        while True:
+            try:
+                # Client message reception
+                data = pickle.loads(recvTopicInfo())
+                print ("Received message from client: ", data.deviceId, data.seqNum, data.msgType, data.timestamp)
+            
+                # Server response
+                msgSeqNum += 1
+                response = None
+                if msgSeqNum < 15:
+                    response = message(msgSeqNum, SERVER_MESSAGE_TYPES[random.randint(0,2)])
+                else:
+                    response = "/0"
+                print ("Response message to client: ", response.seqNum, response.msgType, response.timestamp)
+                serveTopicSubscription(pickle.dumps(response))
+            except Exception as e:
+                print (e)
+                break
+            
+        print ("Closing connection")
+        conn.close()
+except Exception as e:
+    print (e)
+
+# Server termination
+print ("Server application will be terminated...")
+sys.exit()
